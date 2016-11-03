c9696f4e058c0822207e93e5487a96a7269f9fa9
Maciej Adamiak <180122@edu.p.lodz.pl>
2016-11-03 21:08:19 +0100

    simple layout for CPT MobileApp for Android

---
+++ app/src/main/java/com/mobile/cpt/cpt_mobileapp/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.mobile.cpt.cpt_mobileapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_login);
+
+
+    // Example of a call to a native method
+    TextView tv = (TextView) findViewById(R.id.sample_text);
+    tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+}
---
+++ app/src/main/java/com/mobile/cpt/cpt_mobileapp/MainActivity.java
@@ -0,0 +1,9 @@
+package com.mobile.cpt.cpt_mobileapp;
+
+import android.app.Activity;
+
+/**
+ * Created by Makiec on 03.11.2016.
+ */
+public class MainActivity extends Activity {
+}
