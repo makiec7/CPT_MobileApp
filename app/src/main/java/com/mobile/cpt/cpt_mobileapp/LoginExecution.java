011a937fb41839adf87eeabcc329846f8cfb33c5
Maciej Adamiak <180122@edu.p.lodz.pl>
2016-11-13 19:28:17 +0100

    asynctask added

---
+++ app/src/main/java/com/mobile/cpt/cpt_mobileapp/LoginExecution.java
@@ -0,0 +1,77 @@
+package com.mobile.cpt.cpt_mobileapp;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class LoginExecution extends AsyncTask<String, Void, String> {
+
+
+    private Context context;
+
+    public LoginExecution(Context context) {
+        this.context = context;
+    }
+
+    protected void onPreExecute() {
+
+    }
+
+    @Override
+    protected String doInBackground(String... arg0) {
+        String indexNo = arg0[0];
+        String password = arg0[1];
+
+        String link;
+        String data;
+        BufferedReader bufferedReader;
+        String result;
+
+        try {
+            data = "?index_no=" + URLEncoder.encode(indexNo, "UTF-8");
+            data += "&password=" + URLEncoder.encode(password, "UTF-8");
+
+            link = "http://cpt4cti.000webhostapp.com/login.php" + data;
+            URL url = new URL(link);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            result = bufferedReader.readLine();
+            return result;
+        } catch (Exception e) {
+            return new String("Exception: " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        String jsonStr = result;
+        if (jsonStr != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(jsonStr);
+                String query_result = jsonObj.getString("query_result");
+                if (query_result.equals("SUCCESS")) {
+                    Toast.makeText(context, "Data inserted successfully. Signup successfull.", Toast.LENGTH_SHORT).show();
+                } else if (query_result.equals("FAILURE")) {
+                    Toast.makeText(context, "Data could not be inserted. Signup failed.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "Couldn't connect to remote database.", Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Toast.makeText(context, "Error parsing JSON data.", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(context, "Couldn't get any JSON data.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
