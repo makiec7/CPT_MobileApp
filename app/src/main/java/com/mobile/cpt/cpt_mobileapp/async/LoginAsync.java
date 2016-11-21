d54557b549242ee3da374fcb16e7ce1579e390d3
Maciej Adamiak <180122@edu.p.lodz.pl>
2016-11-21 21:07:43 +0100

    PresentAsync refactored dramatically

--- app/src/main/java/com/mobile/cpt/cpt_mobileapp/async/LoginAsync.java
+++ app/src/main/java/com/mobile/cpt/cpt_mobileapp/async/LoginAsync.java
@@ -2,20 +2,16 @@ package com.mobile.cpt.cpt_mobileapp.async;
 
 import android.app.Activity;
 import static com.mobile.cpt.cpt_mobileapp.Constant.*;
-import android.net.ConnectivityManager;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.mobile.cpt.cpt_mobileapp.model.LoginModel;
 
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.InetAddress;
 import java.net.URL;
 import java.net.URLEncoder;
 
@@ -29,48 +25,50 @@ public class LoginAsync extends AsyncTask<String, String, LoginModel> {
 
     @Override
     protected LoginModel doInBackground(String... arg0) {
-        // if (!InetAddress.getByName(HTTP_LOGIN).isReachable(TIMEOUT)) {
-        if (true) {
-            String indexNo = arg0[0];
-            String password = arg0[1];
-            String link;
-            HttpURLConnection conn;
-            BufferedReader bufferedReader;
-            try {
-                link = HTTP_LOGIN;
-                link += ASK + INDEX_NO_EQ + URLEncoder.encode(indexNo, UTF_8);
-                link += AND + PASSWORD_EQ + URLEncoder.encode(password, UTF_8);
-
-                Log.i(LINK, link);
-                URL url = new URL(link);
-                Log.i(URL, url.toString());
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setConnectTimeout(TIMEOUT);
-                conn.setDoInput(true);
-                conn.connect();
-                bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                String jsonStr = bufferedReader.readLine().toString();
-                JSONObject jsonObj = new JSONObject(jsonStr);
-                Log.i(JSON, jsonObj.toString());
-                bufferedReader.close();
-                conn.disconnect();
-                if (jsonStr != null) {
-                    String login_status = (String) jsonObj.get(LOGIN_STATUS);
-                    if (login_status.equals(TRUE)) {
-                        Log.i(IS_LOGGED, login_status);
-                        return new LoginModel(true, (String) jsonObj.get(USER));
-                    } else {
-                        Log.i(IS_LOGGED, login_status);
-                        return new LoginModel(false, NULL_STRING);
-                    }
+        String indexNo = arg0[0];
+        String password = arg0[1];
+        String link;
+        HttpURLConnection conn;
+        BufferedReader bufferedReader;
+        try {
+            link = HTTP_LOGIN;
+            link += ASK + INDEX_NO_EQ + URLEncoder.encode(indexNo, UTF_8);
+            link += AND + PASSWORD_EQ + URLEncoder.encode(password, UTF_8);
+            Log.i(LINK, link);
+            URL url = new URL(link);
+            Log.i(URL, url.toString());
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(TIMEOUT);
+            conn.setDoInput(true);
+            conn.connect();
+            bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String jsonStr = bufferedReader.readLine().toString();
+            JSONObject jsonObj = new JSONObject(jsonStr);
+            Log.i(JSON, jsonObj.toString());
+            bufferedReader.close();
+            conn.disconnect();
+            if (jsonStr != null) {
+                String login_status = (String) jsonObj.get(LOGIN_STATUS);
+                if (login_status.equals(TRUE)) {
+                    Log.i(IS_LOGGED, login_status);
+                    return new LoginModel(true, (String) jsonObj.get(USER));
                 } else {
+                    Log.i(IS_LOGGED, login_status);
                     return new LoginModel(false, NULL_STRING);
                 }
-            } catch (Exception e) {
+            } else {
                 return new LoginModel(false, NULL_STRING);
             }
-        } else {
-            return null;
+        } catch (Exception e) {
+            return new LoginModel(false, NULL_STRING);
         }
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
     }
 }
--- app/src/main/java/com/mobile/cpt/cpt_mobileapp/async/PresentAsync.java
+++ app/src/main/java/com/mobile/cpt/cpt_mobileapp/async/PresentAsync.java
@@ -9,6 +9,7 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -21,20 +22,18 @@ public class PresentAsync extends AsyncTask <String, String, List<FaultModel>> {
 
     @Override
     protected List<FaultModel> doInBackground(String... strings) {
-
-        if (strings.length > 0){
-            return selectUsersFaults(strings[0]);
-        } else {
-            return selectAllFaults();
-        }
-    }
-
-    private List<FaultModel> selectAllFaults() {
-        String link;
-        HttpURLConnection conn;
-        BufferedReader bufferedReader;
         try {
-            link = HTTP_PRESENT_ALL_FAULTS;
+            String link;
+            if (strings.length > 0){
+                link = HTTP_PRESENT_ALL_FAULTS;
+            } else {
+                String user = strings[0];
+                link = HTTP_PRESENT_USER_FAULTS;
+                link += ASK + INDEX_NO_EQ + URLEncoder.encode(user, UTF_8);
+
+            }
+            HttpURLConnection conn;
+            BufferedReader bufferedReader;
             URL url = new URL(link);
             Log.i(URL, url.toString());
             conn = (HttpURLConnection) url.openConnection();
@@ -52,50 +51,13 @@ public class PresentAsync extends AsyncTask <String, String, List<FaultModel>> {
                 if (!faultsJSON.isEmpty()) {
                     return faultsJSON;
                 } else {
-                    return new ArrayList<FaultModel>();
+                    return new ArrayList<>();
                 }
             } else {
-                return new ArrayList<FaultModel>();
+                return new ArrayList<>();
             }
         } catch (Exception e) {
-            return new ArrayList<FaultModel>();
-        }
-    }
-
-    private List<FaultModel> selectUsersFaults(String user){
-        if (true) {
-            String link;
-            HttpURLConnection conn;
-            BufferedReader bufferedReader;
-            try {
-                link = HTTP_PRESENT_USER_FAULTS;
-                link += ASK + INDEX_NO_EQ + URLEncoder.encode(user, UTF_8);
-                URL url = new URL(link);
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setConnectTimeout(TIMEOUT);
-                conn.setDoInput(true);
-                conn.connect();
-                bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                String jsonStr = bufferedReader.readLine().toString();
-                JSONObject jsonObj = new JSONObject(jsonStr);
-                bufferedReader.close();
-                conn.disconnect();
-                if (jsonObj != null) {
-
-                    List<FaultModel> faultsJSON = FaultModel.fromJSONArray(jsonObj);
-                    if (!faultsJSON.isEmpty()) {
-                        return faultsJSON;
-                    } else {
-                        return new ArrayList<FaultModel>();
-                    }
-                } else {
-                    return new ArrayList<FaultModel>();
-                }
-            } catch (Exception e) {
-                return new ArrayList<FaultModel>();
-            }
-        } else {
-            return new ArrayList<FaultModel>();
+            return new ArrayList<>();
         }
     }
 }
