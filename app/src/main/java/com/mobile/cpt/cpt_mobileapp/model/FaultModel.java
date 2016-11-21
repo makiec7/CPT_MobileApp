cd55d49d89773cc010d1e3cf16eaa564e111cd30
Maciej Adamiak <180122@edu.p.lodz.pl>
2016-11-21 20:33:20 +0100

    FaultModel class modified to new model

--- app/src/main/java/com/mobile/cpt/cpt_mobileapp/model/FaultModel.java
+++ app/src/main/java/com/mobile/cpt/cpt_mobileapp/model/FaultModel.java
@@ -7,8 +7,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import static com.mobile.cpt.cpt_mobileapp.Constant.*;
 
@@ -16,53 +19,38 @@ public class FaultModel implements Serializable {
 
     private int id;
     private int issuer;
-    private String phone_no;
+    private String phone_number;
     private String topic;
-    private String obj_name;
+    private String date_time;
     private String description;
-    private int obj_no;
-    private int room_no;
-    private int floor_no;
+    private int object_number;
     private int status;
-    private String handler;
-    private int priority;
 
-    public FaultModel(int issuer, String phone_no, String topic, String obj_name,
-                      String description, int obj_no, int room_no, int floor_no, int status,
-                      String handler, int priority){
+    public FaultModel(int issuer, String phone_number, String topic, String description,
+                      int object_number){
         this.issuer=issuer;
-        this.phone_no=phone_no;
+        this.phone_number=phone_number;
         this.topic=topic;
-        this.obj_name=obj_name;
         this.description=description;
-        this.obj_no=obj_no;
-        this.room_no=room_no;
-        this.floor_no=floor_no;
-        this.status=status;
-        this.handler=handler;
-        this.priority=priority;
+        this.object_number=object_number;
+        this.date_time = getDateTime();
     }
 
-    public FaultModel(int id,int issuer, String phone_no, String topic, String obj_name,
-                      String description, int obj_no, int room_no, int floor_no, int status,
-                      String handler, int priority){
+    public FaultModel(int id, int issuer, String phone_number, String topic, String description,
+                      int object_number, String date_time, int status){
         this.id = id;
         this.issuer=issuer;
-        this.phone_no=phone_no;
+        this.phone_number=phone_number;
         this.topic=topic;
-        this.obj_name=obj_name;
         this.description=description;
-        this.obj_no=obj_no;
-        this.room_no=room_no;
-        this.floor_no=floor_no;
-        this.status=status;
-        this.handler=handler;
-        this.priority=priority;
+        this.object_number=object_number;
+        this.date_time = date_time;
+        this.status = status;
     }
 
     @Override
     public String toString() {
-        return this.issuer+this.phone_no+this.topic+this.obj_name+this.description+this.obj_no+this.room_no+this.floor_no+this.status+this.handler+this.priority;
+        return this.issuer+this.topic+this.description;
     }
 
     public void setDescription(String description) {
@@ -77,36 +65,8 @@ public class FaultModel implements Serializable {
         this.id = id;
     }
 
-    public void setFloor_no(int floor_no) {
-        this.floor_no = floor_no;
-    }
-
-    public void setHandler(String handler) {
-        this.handler = handler;
-    }
-
     public void setIssuer(int issuer) {
         this.issuer = issuer;
-    }
-
-    public void setObj_name(String obj_name) {
-        this.obj_name = obj_name;
-    }
-
-    public void setObj_no(int obj_no) {
-        this.obj_no = obj_no;
-    }
-
-    public void setPhone_no(String phone_no) {
-        this.phone_no = phone_no;
-    }
-
-    public void setPriority(int priority) {
-        this.priority = priority;
-    }
-
-    public void setRoom_no(int room_no) {
-        this.room_no = room_no;
     }
 
     public void setStatus(int status) {
@@ -117,24 +77,8 @@ public class FaultModel implements Serializable {
         this.topic = topic;
     }
 
-    public int getFloor_no() {
-        return floor_no;
-    }
-
     public int getIssuer() {
         return issuer;
-    }
-
-    public int getObj_no() {
-        return obj_no;
-    }
-
-    public int getPriority() {
-        return priority;
-    }
-
-    public int getRoom_no() {
-        return room_no;
     }
 
     public int getStatus() {
@@ -145,29 +89,42 @@ public class FaultModel implements Serializable {
         return description;
     }
 
-    public String getHandler() {
-        return handler;
-    }
-
-    public String getObj_name() {
-        return obj_name;
-    }
-
-    public String getPhone_no() {
-        return phone_no;
-    }
-
     public String getTopic() {
         return topic;
     }
 
+    public int getObject_number() {
+        return object_number;
+    }
+
+    public String getDate_time() {
+        return date_time;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setDate_time(String date_time) {
+        this.date_time = date_time;
+    }
+
+    public void setObject_number(int object_number) {
+        this.object_number = object_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+
     public static FaultModel fromJSONObj(JSONObject jObj){
 
         try {
-            return new FaultModel(jObj.getInt("id"), jObj.getInt("issuer"), jObj.getString("phone_no"),
-                    jObj.getString("topic"), jObj.getString("obj_name"), jObj.getString("description"),
-                    jObj.getInt("obj_no"), jObj.getInt("room_no"), jObj.getInt("floor_no"),
-                    jObj.getInt("status"), jObj.getString("handler"),jObj.getInt("priority"));
+            return new FaultModel(jObj.getInt("id"), jObj.getInt("issuer"),
+                    jObj.getString("phone_number"), jObj.getString("topic"),
+                    jObj.getString("description"), jObj.getInt("object_number"),
+                    jObj.getString("date_time"), jObj.getInt("status"));
         } catch (JSONException e) {
             e.printStackTrace();
             return null;
@@ -187,5 +144,12 @@ public class FaultModel implements Serializable {
             e.printStackTrace();
             return faultModels;
         }
+    }
+
+    private String getDateTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss",
+                Locale.getDefault());
+        Date date = new Date();
+        return dateFormat.format(date);
     }
 }
