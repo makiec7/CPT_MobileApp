4d7e3cde9aba1f7ce279cdde721445eba522cc2b
makiec7 <makiecada@gmail.com>
2016-12-08 12:08:50 +0100

    EditActivity refactor

--- app/src/main/java/com/mobile/cpt/cpt_mobileapp/activity/EditActivity.java
+++ app/src/main/java/com/mobile/cpt/cpt_mobileapp/activity/EditActivity.java
@@ -24,12 +24,17 @@ import static com.mobile.cpt.cpt_mobileapp.Constant.USER_PROBLEMS_LAYOUT;
 
 public class EditActivity extends Activity {
 
+    private List<FaultModel> faults = null;
+    private Intent fromMain;
+    private LoginModel user;
+    private ShortPresentAdapter presentAdapter;
+    private FaultModel fault;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        List<FaultModel> faults = null;
-        Intent fromMain = getIntent();
-        LoginModel user = (LoginModel) fromMain.getExtras().get(USER_DATA);
+        fromMain = getIntent();
+        user = (LoginModel) fromMain.getExtras().get(USER_DATA);
         try {
             if (user.isLogged()) {
                 faults = new PresentAsync().execute(user.getIndex_no()).get();
@@ -46,14 +51,14 @@ public class EditActivity extends Activity {
         this.setContentView(USER_PROBLEMS_LAYOUT);
         final ListView listView = (ListView) findViewById(LIST_FAULT);
         listView.setClickable(true);
-        ShortPresentAdapter presentAdapter = new ShortPresentAdapter(this, SHORT_FAULT_LAYOUT,
+        presentAdapter = new ShortPresentAdapter(this, SHORT_FAULT_LAYOUT,
                 faults);
         listView.setAdapter(presentAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent edition = new Intent(getApplicationContext(), EditFaultActivity.class);
-                FaultModel fault = (FaultModel) listView.getItemAtPosition(i);
+                fault = (FaultModel) listView.getItemAtPosition(i);
                 edition.putExtra(FAULT, fault);
                 startActivityForResult(edition, EDIT_REQUEST_CODE);
             }
