f72730d864bf59b5aaa885a7da489e19ab169f53
makiec7 <makiecada@gmail.com>
2016-12-08 11:44:59 +0100

    MoreInfoActivity refactor

--- app/src/main/java/com/mobile/cpt/cpt_mobileapp/activity/MoreInfoActivity.java
+++ app/src/main/java/com/mobile/cpt/cpt_mobileapp/activity/MoreInfoActivity.java
@@ -19,18 +19,27 @@ import static com.mobile.cpt.cpt_mobileapp.Constant.TV_TOPIC;
 
 public class MoreInfoActivity extends Activity {
 
+    private Intent toPresent;
+    private FaultModel fault;
+    private TextView tv_id;
+    private TextView tv_description;
+    private TextView tv_datetime;
+    private TextView tv_issuer;
+    private TextView tv_obj_no;
+    private TextView tv_topic;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fault_present);
-        Intent toPresent = getIntent();
-        FaultModel fault = (FaultModel) toPresent.getExtras().get(FAULT);
-        TextView tv_id = (TextView) findViewById(TV_ID);
-        TextView tv_description = (TextView) findViewById(TV_DESCRIPTION);
-        TextView tv_datetime = (TextView) findViewById(TV_DATETIME);
-        TextView tv_issuer = (TextView) findViewById(TV_ISSUER);
-        TextView tv_obj_no = (TextView) findViewById(TV_OBJ_NO);
-        TextView tv_topic = (TextView) findViewById(TV_TOPIC);
+        toPresent = getIntent();
+        fault = (FaultModel) toPresent.getExtras().get(FAULT);
+        tv_id = (TextView) findViewById(TV_ID);
+        tv_description = (TextView) findViewById(TV_DESCRIPTION);
+        tv_datetime = (TextView) findViewById(TV_DATETIME);
+        tv_issuer = (TextView) findViewById(TV_ISSUER);
+        tv_obj_no = (TextView) findViewById(TV_OBJ_NO);
+        tv_topic = (TextView) findViewById(TV_TOPIC);
         tv_id.setText(Integer.toString(fault.getId()));
         tv_datetime.setText(fault.getDate_time());
         tv_description.setText(fault.getDescription());
