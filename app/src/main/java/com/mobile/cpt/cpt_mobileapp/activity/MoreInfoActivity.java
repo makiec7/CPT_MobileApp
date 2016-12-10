547401efb5c559e4de4509d5a096e0376d28cb14
Maciej Adamiak <180122@edu.p.lodz.pl>
2016-12-10 12:45:14 +0100

    status in deatiled fault info

--- app/src/main/java/com/mobile/cpt/cpt_mobileapp/activity/MoreInfoActivity.java
+++ app/src/main/java/com/mobile/cpt/cpt_mobileapp/activity/MoreInfoActivity.java
@@ -3,6 +3,7 @@ package com.mobile.cpt.cpt_mobileapp.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mobile.cpt.cpt_mobileapp.R;
@@ -19,32 +20,31 @@ import static com.mobile.cpt.cpt_mobileapp.Constant.TV_TOPIC;
 
 public class MoreInfoActivity extends Activity {
 
-    private Intent toPresent;
-    private FaultModel fault;
-    private TextView tv_id;
-    private TextView tv_description;
-    private TextView tv_datetime;
-    private TextView tv_issuer;
-    private TextView tv_obj_no;
-    private TextView tv_topic;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fault_present);
-        toPresent = getIntent();
-        fault = (FaultModel) toPresent.getExtras().get(FAULT);
-        tv_id = (TextView) findViewById(TV_ID);
-        tv_description = (TextView) findViewById(TV_DESCRIPTION);
-        tv_datetime = (TextView) findViewById(TV_DATETIME);
-        tv_issuer = (TextView) findViewById(TV_ISSUER);
-        tv_obj_no = (TextView) findViewById(TV_OBJ_NO);
-        tv_topic = (TextView) findViewById(TV_TOPIC);
-        tv_id.setText(Integer.toString(fault.getId()));
-        tv_datetime.setText(fault.getDate_time());
-        tv_description.setText(fault.getDescription());
-        tv_issuer.setText(Integer.toString(fault.getIssuer()));
-        tv_obj_no.setText(Integer.toString(fault.getObject_number()));
-        tv_topic.setText(fault.getTopic());
+        Intent toPresent = getIntent();
+        ImageView iv_status = (ImageView) findViewById(R.id.iv_status);
+        FaultModel fault = (FaultModel) toPresent.getExtras().get(FAULT);
+        TextView tv_id = (TextView) findViewById(TV_ID);
+        TextView tv_description = (TextView) findViewById(TV_DESCRIPTION);
+        TextView tv_datetime = (TextView) findViewById(TV_DATETIME);
+        TextView tv_issuer = (TextView) findViewById(TV_ISSUER);
+        TextView tv_obj_no = (TextView) findViewById(TV_OBJ_NO);
+        TextView tv_topic = (TextView) findViewById(TV_TOPIC);
+        if (fault != null) {
+            if (iv_status != null)
+                if (fault.getStatus() == 0)
+                    iv_status.setImageResource(R.drawable.cross_logo);
+                else
+                    iv_status.setImageResource(R.drawable.tick_logo);
+            tv_id.setText(Integer.toString(fault.getId()));
+            tv_datetime.setText(fault.getDate_time());
+            tv_description.setText(fault.getDescription());
+            tv_issuer.setText(Integer.toString(fault.getIssuer()));
+            tv_obj_no.setText(Integer.toString(fault.getObject_number()));
+            tv_topic.setText(fault.getTopic());
+        }
     }
 }
