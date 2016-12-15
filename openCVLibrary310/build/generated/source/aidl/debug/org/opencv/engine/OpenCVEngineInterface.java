3f0c832511e2ba9b44a5647c4afb5e81970a4a8f
Maciej Adamiak <180122@edu.p.lodz.pl>
2016-12-15 20:05:46 +0100

    login and password length added

--- openCVLibrary310/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary310/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\emacada\\StudioProjects\\CPT_MobileApp\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\Makiec\\AndroidStudioProjects\\CPT_MobileApp\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
